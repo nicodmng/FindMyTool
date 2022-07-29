d46237df9c1bbd340fbc5d5fdd59846b322ad643
NIcolas Demange <nicolas.demange@yahoo.fr>
2022-07-29 15:07:01 +0200

    Working on AddToolViewController

--- FindMyTool/Controller/AppView/AddToolViewController.swift
+++ FindMyTool/Controller/AppView/AddToolViewController.swift
@@ -9,9 +9,33 @@ import UIKit
 
 class AddToolViewController: UIViewController {
 
+    // MARK: - Properties
+    let tools = ["Boîte à outils",
+                "Marteau-piqueur",
+                "Tondeuse à gazon",
+                "Taille-haie",
+                "Motoculteur"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
    
+}
+
+// MARK: - Extensions
+extension AddToolViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return tools.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return tools[row]
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
 }
