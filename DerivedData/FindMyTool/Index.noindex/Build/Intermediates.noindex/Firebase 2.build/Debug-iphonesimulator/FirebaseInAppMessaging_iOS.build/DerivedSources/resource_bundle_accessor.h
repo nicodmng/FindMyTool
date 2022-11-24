f13567642d77e5289ffab0dcbc99d2292f3a847c
NIcolas Demange <nicolas.demange@yahoo.fr>
2022-11-24 18:58:00 +0100

    add comments

--- DerivedData/FindMyTool/Index.noindex/Build/Intermediates.noindex/Firebase 2.build/Debug-iphonesimulator/FirebaseInAppMessaging_iOS.build/DerivedSources/resource_bundle_accessor.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-__BEGIN_DECLS
-
-NSBundle* Firebase_FirebaseInAppMessaging_iOS_SWIFTPM_MODULE_BUNDLE(void);
-
-#define SWIFTPM_MODULE_BUNDLE Firebase_FirebaseInAppMessaging_iOS_SWIFTPM_MODULE_BUNDLE()
-
-__END_DECLS
